3fe79d628db4364e020509146ae5e5bb8391d13b
loevc <33386034+loevc@users.noreply.github.com>
2025-12-27 23:37:58 +0800

    feat(v0.0.1): format App.tsx file  [Nadir]

--- src/App.tsx
+++ src/App.tsx
@@ -13,34 +13,34 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite & React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count * 2)}>
-          count is {count} ?
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>} />
-    <Route path="/user" element={<UserPage />} />
-    <Route path='/test' element={
-      <>
-      <TestPlain title='test_name' size={2} render={() => <div style={{color : 'red'}}>render fx</div>} />
-      <TestPlain title='test_name' size={2} render={() => <div style={{color : 'red'}}>render fx</div>} />
-      </>
-      } />
+          <div>
+            <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
+              <img src={viteLogo} className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+          </div>
+          <h1>Vite & React</h1>
+          <div className="card">
+            <button onClick={() => setCount((count) => count * 2)}>
+              count is {count} ?
+            </button>
+            <p>
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+          </p>
+        </>} />
+        <Route path="/user" element={<UserPage />} />
+        <Route path='/test' element={
+          <>
+            <TestPlain title='test_name' size={2} render={() => <div style={{ color: 'red' }}>render fx</div>} />
+            <TestPlain title='test_name' size={2} render={() => <div style={{ color: 'red' }}>render fx</div>} />
+          </>
+        } />
       </Routes>
     </BrowserRouter>
   )
