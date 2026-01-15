9f7b5c48a25c33acf2a6ffc55e5b41833ef6776d
loevc <33386034+loevc@users.noreply.github.com>
2026-01-15 11:33:26 +0800

    feat(v0.0.1):  add recharge portal  [Nadir]

--- src/pages/make_love/index.tsx
+++ src/pages/make_love/index.tsx
@@ -1,10 +1,42 @@
 import { useEffect } from "react";
+import { useState } from "react";
 import toast from "react-hot-toast";
 
+
+interface User {
+  id: number;
+  name: string;
+  title: string;
+  avatar: string;
+  isMatched?: boolean;
+}
 
 export default function MakeLovePage() {
 
     const isMember = false;
+
+    const [users, setUsers] = useState<User[]>([
+        {
+            id: 1,
+            name: "Erin Lindford",
+            title: "Product Engineer",
+            avatar: "https://tailwindcss.com/_next/static/media/erin-lindford.90b9d461.jpg"
+        },
+        {
+            id: 2,
+            name: "Arlene McCoy",
+            title: "UI Designer",
+            avatar: "https://images.unsplash.com/photo-1494790108755-2616b612b786"
+        },
+        {
+            id: 3,
+            name: "Courtney Henry",
+            title: "Backend Developer",
+            avatar: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80"
+        }
+    ]);
+    const [isMatching, setIsMatching] = useState(false);
+    const [matchedUser, setMatchedUser] = useState<User | null>(null);
 
     const handleDoIt = () => {
         console.log("do once 11");
@@ -19,9 +51,20 @@ export default function MakeLovePage() {
     };
 
     const handleWait = () => {
-        alert("等个锤子🔨")
+        toast.error("🔨请耐心等待💤", {
+            duration: 4000,
+            position: "top-center",
+        });
         console.log("waitting");
     };
+
+    const charge = () => {
+        toast.success("充值成功！💖", {
+            duration: 4000,
+            position: "bottom-left",
+        });
+        console.log("charge");
+    }
 
     const handleTips = () => {
         alert("注意：本页面仅供娱乐，请勿模仿！")
@@ -30,13 +73,33 @@ export default function MakeLovePage() {
 
     return (
         <>
-            <div className="flex flex-col text-pink-500  ">
-                <h1>整了吗</h1>   
+            <div className="fixed top-4 right-4 gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
+                <img className="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0" src="https://tailwindcss.com/_next/static/media/erin-lindford.90b9d461.jpg" alt="" />
+                <div className="space-y-2 text-center sm:text-left">
+                    <div className="space-y-0.5">
+                        <p className="text-lg font-semibold text-black">Erin Lindford</p>
+                        <p className="font-medium text-gray-500">Product Engineer</p>
+                    </div>
+                    <button className="border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 ...">
+                        DM
+                    </button>
+                </div>
+            </div>
+
+
+            <div className="flex flex-col text-green-500  ">
+                <h1>整了么</h1>
             </div>
             <div className="flex flex-row">
-                <button className="text-green-500 mr-4" onClick={handleDoIt}>整一下</button>
+                <button className="text-pink-500 mr-4" onClick={handleDoIt}>整一下</button>
                 <button className="text-red-500 ml-4" onClick={handleWait}>再等等</button>
             </div>
+
+
+            <div>
+                <button className="fixed bottom-4 left-4 text-red-500 ml-4" onClick={charge}>充值入口</button>
+            </div>
+
             <div>
                 <p className="fixed bottom-4 right-4 text-gray-500 " onClick={handleTips}>注意：本页面仅供娱乐，请勿模仿！</p>
             </div>
